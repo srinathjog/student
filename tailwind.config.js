01ce03c2b362918f91dee49c5e7336593b932686
Srinath Jog <srinath.jog@in.ey.com>
2025-09-06 08:33:13 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",   // ✅ Angular HTML + component TS files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
